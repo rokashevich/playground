90333ffce096e8475fb05203a8080af97cb7ef6d
Aleksandr Rokashevich <rokashevich@gmail.com>
2020-01-01 23:33:49 +0300

    -

--- cpp/pointer.cpp
+++ cpp/pointer.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
 int main() {
+	char ar_char[] = {'a', 'b', 'c'};
+	short ar_short[] = {1, 2, 3};
 	int ar_int[] = {1, 2, 3};
-	std::cout << "ar_int" << sizeof(ar_int) << std::endl;
+	long ar_long[] = {1, 2, 3};
+	long long ar_long_long[] = {1, 2, 3};
+	std::cout << "ar_char      " << sizeof(ar_char) << std::endl;
+	std::cout << "ar_short     " << sizeof(ar_short) << std::endl;
+	std::cout << "ar_int       " << sizeof(ar_int) << std::endl;
+	std::cout << "ar_long      " << sizeof(ar_long) << std::endl;
+	std::cout << "ar_long_long " << sizeof(ar_long_long) << std::endl;
 	return 0;
 }
