14efb54321206004cdc846859f8751f79e82d773
Aleksandr Rokashevich <rokashevich@gmail.com>
2020-01-07 22:38:50 +0300

    sizeof

---
+++ cpp/types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	std::cout << "bool        sizeof=" << sizeof(bool) << std::endl;
+	std::cout << "char        sizeof=" << sizeof(char) << std::endl;
+	std::cout << "short       sizeof=" << sizeof(short) << std::endl;
+	std::cout << "int         sizeof=" << sizeof(int) << std::endl;
+	std::cout << "long        sizeof=" << sizeof(long) << std::endl;
+	std::cout << "long long   sizeof=" << sizeof(long long) << std::endl;
+	std::cout << "float       sizeof=" << sizeof(float) << std::endl;
+	std::cout << "double      sizeof=" << sizeof(double) << std::endl;
+	std::cout << "long double sizeof=" << sizeof(long double) << std::endl;
+	return 0;
+}
