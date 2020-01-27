74e4a6fe68cb6c9fcf338a3698c931bcd98d88ab
Aleksander Rokashevich <rokashevich@gmail.com>
2020-01-27 17:55:04 +0300

    Программное выделение произвольной строки в qtableview

--- qt/qtableview/mainwindow.cpp
+++ qt/qtableview/mainwindow.cpp
@@ -26,8 +26,26 @@ MainWindow::MainWindow(QWidget *parent)
             qDebug() << "selected row =" << selected1.indexes().at(0).row();
           });
 
-  connect(ui->tableView,&QTableView::doubleClicked,this,[](const QModelIndex &index) {
-    qDebug() << "Двойной клик в клетке" << index.row() << "," << index.column();
+  connect(ui->tableView, &QTableView::doubleClicked, this,
+          [](const QModelIndex &index) {
+            qDebug() << "Двойной клик в клетке" << index.row() << ","
+                     << index.column();
+          });
+
+  connect(ui->buttonSelectDown, &QPushButton::clicked, this, [this]() {
+    QItemSelectionModel *sm = ui->tableView->selectionModel();
+    int selected_row = -1;
+    int selected_rows = sm->selection().indexes().count();
+    if (selected_rows > 0) {
+      selected_row = sm->selection().indexes().at(0).row();
+    }
+    int max_row = ui->tableView->model()->rowCount() - 1;
+    int next_row = 0;
+    if (selected_row < max_row) {
+      next_row = selected_row + 1;
+    }
+    QModelIndex next_index = ui->tableView->model()->index(next_row, 0);
+    ui->tableView->setCurrentIndex(next_index);
   });
 }
 
