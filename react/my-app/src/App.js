7d44d60f248274fe59f9d12c2b7da8182ad32923
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-01 11:29:08 +0300

    11 урок

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,28 +1,36 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
-const Header = () => {
-  return <div><a href="#s">link1</a>
-    <a href="#s">link2</a>
-    <a href="#s">link3</a></div>
-}
-
-const Technologies = () => {
-  return <div>
-    <ul>
-      <li>t1</li>
-      <li>t2</li>
-      <li>t3</li>
-    </ul>
-  </div>
-}
+import Header from './Header'
+import Technologies from './Technologies'
 
 const App = () => {
-  return <div>
-    <Header />
-    <Technologies />
-  </div>
-
+  return <div className='app-wrapper'>
+    <header className='header'>
+      <img src='https://yt3.ggpht.com/ytc/AAUvwnhcOYdTa8LLrCrXBvmGL2kd1pP1xiGLChGUaz4KGA=s48-c-k-c0x00ffffff-no-rj'></img>
+    </header>
+    <nav className='nav'>
+      <div><a>Profile</a></div>
+      <div><a>Messages</a></div>
+      <div><a>News</a></div>
+      <div><a>Music</a></div>
+      <div><a>Settings</a></div>
+    </nav>
+    <div className='content'>
+      <div>
+        <img height='100px' src='https://wallpapercave.com/wp/wp2173645.jpg'></img>
+      </div>
+      <div>ava + desc</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+          <div>post 3</div>
+        </div>
+      </div>
+    </div>
+  </div >
 }
 
 export default App;
---
+++ react/my-app/src/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <div><a href="#s">link1</a> -
+        <a href="#s">link2</a> -
+        <a href="#s">link3</a></div>
+}
+
+export default Header;
