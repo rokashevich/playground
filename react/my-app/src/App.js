ea15554a3257fae615cad39ff9b300777d70ccaf
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-07 18:58:42 +0300

    19

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import './App.css';
-import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
-import Profile from './components/Profile/Profile'
-import Dialogs from './components/Dialogs/Dialogs'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
-  return <div className='app-wrapper'>
-    <Header />
-    <Navbar />
-    <div class='app-wrapper-content'>
-      {/* <Profile /> */}
-      <Dialogs />
-    </div>
-  </div >
-}
+  return (
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div class='app-wrapper-content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          {/* <Profile /> */}
+          {/* <Dialogs /> */}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
--- react/my-app/src/components/Navbar/Navbar.jsx
+++ react/my-app/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import s from './Navbar.module.css'
-console.log(s)
+import React from "react";
+import s from "./Navbar.module.css";
+console.log(s);
 
 const Navbar = () => {
-    return     <nav className={s.nav}>
-    <div className={s.item}><a>Profile</a></div>
-    <div className={`${s.item} ${s.active}`}><a>Messages</a></div>
-    <div className={s.item}><a>News</a></div>
-    <div className={s.item}><a>Music</a></div>
-    <div className={s.item}><a>Settings</a></div>
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <a href='/profile'>Profile</a>
+      </div>
+      <div className={s.item}>
+        <a href='/dialogs'>Dialogs</a>
+      </div>
+      <div className={`${s.item} ${s.active}`}>
+        <a>Messages</a>
+      </div>
+      <div className={s.item}>
+        <a href='/news'>News</a>
+      </div>
+      <div className={s.item}>
+        <a href='/music'>Music</a>
+      </div>
+      <div className={s.item}>
+        <a href='/settings'>Settings</a>
+      </div>
 
-    <div className={s.b}><a>b1</a></div>
-    <div className={`${s.b} ${s.active}`}><a>b2</a></div>
-  </nav>
-}
+      <div className={s.b}>
+        <a>b1</a>
+      </div>
+      <div className={`${s.b} ${s.active}`}>
+        <a>b2</a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
