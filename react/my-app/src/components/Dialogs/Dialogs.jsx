12decf28e7bafe44b61fef266a628d9c8b693069
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-09 18:45:28 +0300

    25

--- react/my-app/src/components/Dialogs/Dialogs.jsx
+++ react/my-app/src/components/Dialogs/Dialogs.jsx
@@ -13,36 +13,27 @@ const Message = props => {
   return <div className={s.message}>{props.message}</div>
 }
 const Dialogs = props => {
-  let dialogsData = [
+  let dialogs = [
     { id: 1, name: 'Dimych' },
     { id: 2, name: 'Andrey' },
     { id: 3, name: 'Sveta' },
     { id: 4, name: 'Sasha' },
     { id: 5, name: 'Viktor' }
   ]
-  let messagesData = [
+  let dialogsElements = dialogs.map(e => <DialogItem name={e.name} id={e.id} />)
+
+  let messages = [
     { id: 1, message: 'Hi' },
     { id: 2, message: 'LOL' },
     { id: 3, message: 'Hellow' },
     { id: 4, message: 'Eee!' },
     { id: 5, message: 'Привет!' }
   ]
+  let messagesElements = messages.map(e => <Message message={e.message} />)
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogsItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-      </div>
-      <div className={s.messages}>
-        <Message message={messagesData[0].message} />
-        <Message message={messagesData[1].message} />
-        <Message message={messagesData[2].message} />
-        <Message message={messagesData[3].message} />
-        <Message message={messagesData[4].message} />
-      </div>
+      <div className={s.dialogsItems}>{dialogsElements}</div>
+      <div className={s.messages}>{messagesElements}</div>
     </div>
   )
 }
--- react/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ react/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,11 +3,15 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = () => {
-  let postsData = [
+  let posts = [
     { id: 1, message: 'Hi, how are you?', likesCount: 10 },
     { id: 2, message: "It's my first post", likesCount: 12 },
-    { id: 3, message: 'Second post!', likesCount: 1000 }
+    { id: 3, message: 'Second post!', likesCount: 1000 },
+    { id: 4, message: 'Fourth post!', likesCount: 4 }
   ]
+  let postsElements = posts.map(e => (
+    <Post message={e.message} likesCount={e.likesCount} />
+  ))
   return (
     <div className={s.postsBlock}>
       <h3>My Posts</h3>
@@ -19,20 +23,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        <Post
-          message={postsData[0].message}
-          likesCount={postsData[0].likesCount}
-        />
-        <Post
-          message={postsData[1].message}
-          likesCount={postsData[1].likesCount}
-        />
-        <Post
-          message={postsData[2].message}
-          likesCount={postsData[2].likesCount}
-        />
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   )
 }
