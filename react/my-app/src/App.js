720f96ca22170ae161449d9c0f281fd27a8115c5
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-09 19:40:36 +0300

    27

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Profile from './components/Profile/Profile'
+import Dialogs from './components/Dialogs/Dialogs'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -13,12 +13,12 @@ const App = () => {
         <Header />
         <Navbar />
         <div class='app-wrapper-content'>
-          <Route path='/profile' component={Profile} />
-          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={() => <Profile />} />
+          <Route path='/dialogs' component={() => <Dialogs />} />
         </div>
       </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
