4d469f3e5a99f5a337a5d8438e51ce61766bc7c4
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-10 19:04:13 +0300

    33

--- react/my-app/src/redux/state.js
+++ react/my-app/src/redux/state.js
@@ -1,3 +1,4 @@
+import { rerenderEntireTree } from '../render'
 let state = {
   profilePage: {
     posts: [
@@ -31,5 +32,6 @@ export let addPost = postMessage => {
     message: postMessage
   }
   state.profilePage.posts.push(newPost)
+  rerenderEntireTree(state)
 }
 export default state
