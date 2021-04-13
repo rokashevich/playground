01f70f1a45cdbb3b39ce729428617a2678fd970f
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-13 23:08:36 +0300

    37

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,27 +1,26 @@
 import './index.css'
 import reportWebVitals from './reportWebVitals'
-import state, { subscribe } from './redux/state'
+import store from './redux/state'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { addPost, updateNewPostText } from './redux/state'
 
 let rerenderEntireTree = state => {
   ReactDOM.render(
     <React.StrictMode>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById('root')
   )
 }
-rerenderEntireTree(state)
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- react/my-app/src/redux/state.js
+++ react/my-app/src/redux/state.js
@@ -1,54 +1,56 @@
-let rerenderEntireTree = () => {
-  console.log('state changed')
-}
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, how are you?', likesCount: 10 },
-      { id: 2, message: "It's my first post", likesCount: 12 },
-      { id: 3, message: 'Second post!', likesCount: 1000 },
-      { id: 4, message: 'Fourth post!', likesCount: 4 }
-    ],
-    newPostText: 'it-camasutra.com'
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 10 },
+        { id: 2, message: "It's my first post", likesCount: 12 },
+        { id: 3, message: 'Second post!', likesCount: 1000 },
+        { id: 4, message: 'Fourth post!', likesCount: 4 }
+      ],
+      newPostText: 'it-camasutra.com'
+    },
+    dialogsPage: {
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'LOL' },
+        { id: 3, message: 'Hellow' },
+        { id: 4, message: 'Eee!' },
+        { id: 5, message: 'Привет!' }
+      ],
+      dialogs: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Viktor' }
+      ]
+    }
   },
-  dialogsPage: {
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'LOL' },
-      { id: 3, message: 'Hellow' },
-      { id: 4, message: 'Eee!' },
-      { id: 5, message: 'Привет!' }
-    ],
-    dialogs: [
-      { id: 1, name: 'Dimych' },
-      { id: 2, name: 'Andrey' },
-      { id: 3, name: 'Sveta' },
-      { id: 4, name: 'Sasha' },
-      { id: 5, name: 'Viktor' }
-    ]
+  getState() {
+    return this._state
+  },
+  _callSubscriber() {
+    console.log('state changed')
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this.getState().profilePage.newPostText,
+      likesCount: 0
+    }
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText = ''
+    this._callSubscriber(this._state)
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText
+    this._callSubscriber(this._state)
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer
   }
 }
 
-window.state = state
+window.store = store
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  }
-  state.profilePage.posts.push(newPost)
-  state.profilePage.newPostText = ''
-  rerenderEntireTree(state)
-}
-
-export const subscribe = observer => {
-  rerenderEntireTree = observer
-}
-
-export let updateNewPostText = newText => {
-  state.profilePage.newPostText = newText
-  rerenderEntireTree(state)
-}
-
-export default state
+export default store
