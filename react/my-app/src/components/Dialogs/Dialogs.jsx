4c570f90a98526858036dd0be55de6a6f1625d5a
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-14 18:52:05 +0100

    41

--- react/my-app/src/components/Dialogs/Dialogs.jsx
+++ react/my-app/src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,7 @@ import s from './Dialogs.module.css'
 import {
   sendMessageCreator,
   updateNewMessageBodyCreator
-} from '../../redux/state'
+} from '../../redux/dialogs-reducer'
 
 const Dialogs = props => {
   let state = props.store.getState().dialogsPage
