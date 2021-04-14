d71b7d4065f005907e22da342370d9e6fed998b7
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-14 17:10:54 +0100

    удалил варнинги

--- react/my-app/src/components/Profile/MyPosts/Post/Post.jsx
+++ react/my-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,9 @@ import s from './Post.module.css'
 const Post = props => {
   return (
     <div className={s.item}>
-      <img src='https://img1.looper.com/img/gallery/robocop-sequel-loses-director-neill-blomkamp/intro-1565879277.jpg'></img>
+      <img
+        src='https://img1.looper.com/img/gallery/robocop-sequel-loses-director-neill-blomkamp/intro-1565879277.jpg'
+        alt='ava'></img>
       {props.message}
       <span>❤{props.likesCount}</span>
     </div>
