779d329262b39184af0febce9d1aed1b86d9eee6
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-04-22 05:38:35 +0100

    Убрал Header чтоб больше влезало всего

--- react/my-app/src/components/Header/Header.jsx
+++ react/my-app/src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import s from './Header.module.css'
 const Header = () => {
   return (
     <header className={s.header}>
-      <img
+      {/* <img
         alt='alternative text'
-        src='https://yt3.ggpht.com/ytc/AAUvwnhcOYdTa8LLrCrXBvmGL2kd1pP1xiGLChGUaz4KGA=s48-c-k-c0x00ffffff-no-rj'></img>
+        src='https://yt3.ggpht.com/ytc/AAUvwnhcOYdTa8LLrCrXBvmGL2kd1pP1xiGLChGUaz4KGA=s48-c-k-c0x00ffffff-no-rj'></img> */}
     </header>
   )
 }
