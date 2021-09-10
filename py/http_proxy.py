143691a57c6a56c7d8e6531d7bfaa6636cb25cda
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-09-10 13:59:37 +0300

    simple python3 http_proxy?

--- py/http_proxy.py
+++ py/http_proxy.py
@@ -1,11 +1,12 @@
 import socketserver
 import http.server
-import urllib
+import urllib.request
 
 
 class Proxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.copyfile(urllib.urlopen(self.path), self.wfile)
+        print(self.path)
+        self.copyfile(urllib.request.urlopen(self.path), self.wfile)
 
 
 httpd = socketserver.ForkingTCPServer(('', 3128), Proxy)
