068e009857817751852812ad22e6110c80123a48
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-10-19 15:28:18 +0300

    clang-format on

--- cpp/boost/httpd.cpp
+++ cpp/boost/httpd.cpp
@@ -2,6 +2,7 @@
 // g++ echo-client-server.cpp -lpthread -lboost_system -lboost_thread -lboost_program_options
 // g++ httpd.cpp -lpthread -lboost_system -o httpd
 // https://www.codeproject.com/articles/1007262/a-minimal-http-web-server-using-boost-asio
+// clang-format on
 
 #include <boost/asio.hpp>
 #include <iostream>
@@ -12,21 +13,25 @@ using namespace boost::asio;
 
 class session {
   asio::streambuf buff;
-public:
+
+ public:
   static void read_first_line(std::shared_ptr<session> pThis) {
-    std::cout << "async_read_until(sock, buff, read_callback) // register read_callback\n";
+    std::cout << "async_read_until(sock, buff, read_callback) // register "
+                 "read_callback\n";
     // GET запрос имеет вид "GET /script.sh HTTP/1.1\r\n"
-    asio::async_read_until(pThis->socket, pThis->buff, '\r',
-                           [pThis](const error_code& e, std::size_t s) {
-                             std::cout << "\read_callback begin\n";
-                             std::string line;
-                             std::istream stream{&pThis->buff};
-                             std::getline(stream, line, '\r');
-                             // ТУТ НАДО СФОРМИРОВАТЬ ПРАВИЛЬНЫЙ ЗАГОЛОВОК И ЗАПИСАТЬ В СОКЕТ
-                             asio::async_write(pThis->socket, boost::asio::buffer(line.c_str(), line.length()),
-                             [](const error_code& e, std::size_t s){});
-                             std::cout << "\read_callback end\n";
-                           });
+    asio::async_read_until(
+        pThis->socket, pThis->buff, '\r',
+        [pThis](const error_code& e, std::size_t s) {
+          std::cout << "\read_callback begin\n";
+          std::string line;
+          std::istream stream{&pThis->buff};
+          std::getline(stream, line, '\r');
+          // ТУТ НАДО СФОРМИРОВАТЬ ПРАВИЛЬНЫЙ ЗАГОЛОВОК И ЗАПИСАТЬ В СОКЕТ
+          asio::async_write(pThis->socket,
+                            boost::asio::buffer(line.c_str(), line.length()),
+                            [](const error_code& e, std::size_t s) {});
+          std::cout << "\read_callback end\n";
+        });
   }
   ip::tcp::socket socket;
   session(io_service& io_service) : socket(io_service) {}
@@ -34,7 +39,8 @@ public:
 
 void accept_and_run(ip::tcp::acceptor& acceptor, io_service& io_service) {
   std::shared_ptr<session> sesh = std::make_shared<session>(io_service);
-  std::cout << "acceptor.async_accept(sock, accept_callback) // register callback\n";
+  std::cout
+      << "acceptor.async_accept(sock, accept_callback) // register callback\n";
   acceptor.async_accept(sesh->socket, [sesh, &acceptor, &io_service](
                                           const error_code& accept_error) {
     std::cout << "\naccept_callback begin\n";
