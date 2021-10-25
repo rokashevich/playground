28dde94d3645a444eeb25c85748ceae2951124ef
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-10-25 09:28:44 +0300

    -

--- cpp/pointer.cpp
+++ cpp/pointer.cpp
@@ -3,23 +3,36 @@
 void f(int *&) {}
 
 int main() {
-  char *label{"xxxx"};
-  label[1] = 'y';
-  std::cout << std::string(label) << '\n';
+  int i = 0;
+  int j = 0;
+  std::cout << ++i + ++j << '\n';  // 2|Приоритеты: ++x,--x=2;
+  std::cout << i++ + j++ << '\n';  // 2|x++,x--=3;*/=5,+/-=6, <</>>=7
+  // std::cout << ++i++ << '\n';  // error: lvalue required as increment operand
+  // std::cout << ++(i++) << '\n'; // тоже самое
+  std::cout << (++i)++ << '\n';  // 3
 
-  // int *p;
-  // f(p);
-  // std::cout << *p << '\n';
+  if (0) {
+    // warning: ISO C++ forbids converting a string constant to ‘char*’
+    // char *label{"xxxx"}; // BAD
+    const char *label{"xxxx"};
+    // label[1] = 'y'; // Так соотвественно нельзя т.к. const
 
-  char ar_char[] = {'a', 'b', 'c'};
-  short ar_short[] = {1, 2, 3};
-  int ar_int[] = {1, 2, 3};
-  long ar_long[] = {1, 2, 3};
-  long long ar_long_long[] = {1, 2, 3};
-  std::cout << "ar_char      " << sizeof(ar_char) << std::endl;
-  std::cout << "ar_short     " << sizeof(ar_short) << std::endl;
-  std::cout << "ar_int       " << sizeof(ar_int) << std::endl;
-  std::cout << "ar_long      " << sizeof(ar_long) << std::endl;
-  std::cout << "ar_long_long " << sizeof(ar_long_long) << std::endl;
+    std::cout << std::string(label) << '\n';
+
+    // int *p;
+    // f(p);
+    // std::cout << *p << '\n';
+
+    char ar_char[] = {'a', 'b', 'c'};
+    short ar_short[] = {1, 2, 3};
+    int ar_int[] = {1, 2, 3};
+    long ar_long[] = {1, 2, 3};
+    long long ar_long_long[] = {1, 2, 3};
+    std::cout << "ar_char      " << sizeof(ar_char) << std::endl;
+    std::cout << "ar_short     " << sizeof(ar_short) << std::endl;
+    std::cout << "ar_int       " << sizeof(ar_int) << std::endl;
+    std::cout << "ar_long      " << sizeof(ar_long) << std::endl;
+    std::cout << "ar_long_long " << sizeof(ar_long_long) << std::endl;
+  }
   return 0;
 }
