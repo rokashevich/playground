f52f328c602b2fad3bb93983eb2d3c9488f8edb3
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-10-25 14:56:41 +0300

    copy-and-swap

--- cpp/naive/copy-and-swap.cpp
+++ cpp/naive/copy-and-swap.cpp
@@ -1,16 +1,24 @@
+// clang-format off
 // https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
 #include <iostream>
-
+#include <algorithm>
 class C {
   std::size_t size_;
   int* array_;
 
  public:
-  C(std::size_t size = 0)
-      : size_(size), array_(size_ ? new int[size_] : nullptr) {}
-  ~C() { delete[] array_; }
-  C(C& other) {}
-  C& operator=(C& other) {
+  C(std::size_t size = 0) //default constructor
+      : size_{size}, array_{size_ ? new int[size_] : nullptr} {}
+  C(const C& other) //copy-constructor
+      : size_{other.size_}, array_{size_?new int[size_] : nullptr} {
+    std::copy(other.array_, other.array_ + size_, array_);
+  }
+  C(C&& other) noexcept //move-constructor
+      : C() {
+    swap(*this, other);
+  }
+  ~C() { delete[] array_; } //destructor
+  C& operator=(C other) {
     swap(*this, other);
     return *this;
   }
@@ -19,3 +27,5 @@ class C {
     std::swap(a.array_, b.array_);  // non-throwing
   }
 };
+
+int main(){return 0;}
